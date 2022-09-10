89f06f5c87db69077a204f2740bc2812fc47b6d6
Adam Cierniak <ad.cierniak@gmail.com>
2022-09-10 14:59:45 +0200

    configure eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard-with-typescript',
+    'eslint:recommended',
+    'plugin:prettier/recommended'
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json']
+  },
+  plugins: ['react'],
+  rules: {}
+};
