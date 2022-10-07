6600e7ebfd0c46066af88ff3dba98395d0ad05f2
Wojciech Nowicki <95188715+wkn12@users.noreply.github.com>
2022-10-07 16:53:52 +0200

    Adjustments after merge

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -6,11 +6,7 @@ import { Dashboard } from 'features/dashboard';
 import { DataProvider } from 'features/assets';
 import { Layout } from 'components/Layout';
 import { Login } from 'features/login';
-<<<<<<< HEAD
-import AddAsset from 'features/Forms/AddAsset/AddAsset';
-=======
 import AddAsset from 'features/assets/components/AddAsset';
->>>>>>> 34e97a1c59a41b3ae4911bb2da9d51cd55f09b07
 
 export const AppRoutes = (): JSX.Element => (
   <Routes>
