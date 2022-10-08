6025e6b064d816048f1e64cdd110ffe71a82a111
Wojciech Nowicki <95188715+wkn12@users.noreply.github.com>
2022-10-09 00:59:27 +0200

    AddAsset loading,setValues funciton,EditAsset test

--- src/features/assets/components/AddAsset.tsx
+++ src/features/assets/components/AddAsset.tsx
@@ -21,9 +21,10 @@ import {
   TextInput,
 } from 'components/Elements/FormInputs';
 import { useQuery } from 'react-query';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import testData from '../api/testData.json';
 import moment from 'moment';
+import { LoadingScreen } from 'components/Elements/Loading';
 
 const AddAsset = () => {
   const methods = useForm<IFormInput>();
@@ -34,11 +35,30 @@ const AddAsset = () => {
   const location = useLocation();
   const { id } = useParams();
   const [action, setAction] = useState<'Add' | 'Edit'>('Add');
+  const [loading, setLoading] = useState<boolean>(false);
 
   const getDateFormat = (dateString: string) => {
     const dateMoment = moment(dateString, 'DD/MM/YYYY');
     return dateMoment.toDate();
   };
+
+  const setValues = useCallback(
+    (asset: any) => {
+      methods.setValue('AssetTag', asset.assetTag);
+      methods.setValue('Serial', asset.serial);
+      const modelObject = modelOptions?.find((x) => x.name === asset.model);
+      methods.setValue('Model', modelObject !== undefined ? modelObject : null);
+      const statusObject = statusOptions?.find((x) => x.name === asset.status);
+      methods.setValue('Status', statusObject !== undefined ? statusObject : null);
+      methods.setValue('Notes', asset.notes);
+      methods.setValue('AssetName', asset.name);
+      methods.setValue('Waranty', asset.waranty);
+      methods.setValue('OrderNumber', asset.orderNumber);
+      methods.setValue('DateOfPurchase', getDateFormat(asset.dateOfPurchase));
+      methods.setValue('PurchaseCost', asset.purchaseCost);
+    },
+    [methods, modelOptions, statusOptions],
+  );
 
   useEffect(() => {
     const isEdit = location.pathname.includes('EditAsset');
@@ -49,27 +69,18 @@ const AddAsset = () => {
     }
 
     if (isEdit && id !== undefined) {
+      setLoading(true);
       setAction('Edit');
       // TEST - REPLACE BY API CALL
       const asset = testData.find((x) => {
         if (x.id === Number(id)) return x;
       });
       if (asset !== undefined) {
-        methods.setValue('AssetTag', asset.assetTag);
-        methods.setValue('Serial', asset.serial);
-        const modelObject = modelOptions?.find((x) => x.name === asset.model);
-        methods.setValue('Model', modelObject !== undefined ? modelObject : null);
-        const statusObject = statusOptions?.find((x) => x.name === asset.status);
-        methods.setValue('Status', statusObject !== undefined ? statusObject : null);
-        methods.setValue('Notes', asset.notes);
-        methods.setValue('AssetName', asset.name);
-        methods.setValue('Waranty', asset.waranty);
-        methods.setValue('OrderNumber', asset.orderNumber);
-        methods.setValue('DateOfPurchase', getDateFormat(asset.dateOfPurchase));
-        methods.setValue('PurchaseCost', asset.purchaseCost);
+        setValues(asset);
+        setLoading(false);
       }
     }
-  }, [id, location.pathname, methods, modelOptions, navigate, statusOptions]);
+  }, [id, location.pathname, methods, navigate, setValues, statusOptions]);
 
   const onSubmit = (data: IFormInput) => {
     console.log(data);
@@ -87,141 +98,151 @@ const AddAsset = () => {
         marginTop: 2,
       }}
     >
-      <Grid alignItems="center" container justifyContent="start" pt={2} spacing={0}>
-        <Grid item lg={6} md={6} sm={6} xl={6} xs={6}>
-          <Typography ml={2} variant="h4">
-            {action} Asset
-          </Typography>
-        </Grid>
-        <Grid
-          alignContent="center"
-          display="flex"
-          item
-          justifyContent="flex-end"
-          lg={6}
-          md={6}
-          sm={6}
-          xl={6}
-          xs={6}
-        >
-          <Stack direction="row" mb={1} mr={2} spacing={1}>
-            <Button color="error" onClick={() => navigate(-1)} variant="contained">
-              Cancel
-            </Button>
-            <Button color="success" onClick={handleSubmit(onSubmit)} variant="contained">
-              {action}
-            </Button>
-          </Stack>
-        </Grid>
-      </Grid>
-      <Divider />
-      <FormProvider {...methods}>
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          style={{
-            width: '100%',
-          }}
-        >
-          <Grid alignContent="center" container display="flex" item mt={2} spacing={2}>
-            <TextInput label="Asset Tag" name="AssetTag" rules={{ required: 'Required value' }} />
-            <TextInput label="Serial" name="Serial" rules={{ required: 'Required value' }} />
-            <SelectInput
-              label="Model"
-              name="Model"
-              containsImg
-              options={modelOptions ? modelOptions : []}
-            />
-            <SelectInput
-              label="Status"
-              name="Status"
-              options={statusOptions ? statusOptions : []}
-            />
-            <Grid item lg={12} md={12} sm={12} xl={12} xs={12}>
-              <Accordion disableGutters>
-                <AccordionSummary
-                  aria-controls="panel1a-content"
-                  expandIcon={<ExpandMoreIcon />}
-                  id="panel1a-header"
-                  sx={{
-                    backgroundColor: 'background.default',
-                  }}
-                >
-                  <Typography>Additional informations</Typography>
-                </AccordionSummary>
-                <AccordionDetails
-                  sx={{
-                    backgroundColor: 'background.paper',
-                  }}
-                >
-                  <Grid alignContent="center" container display="flex" spacing={2}>
-                    <MultiLineTextInput label="Notes" name="Notes" rows={4} />
-                    <UploadImage buttonText="Upload photo" name="Photo" />
-                    <TextInput label="Asset Name" name="AssetName" rules={{}} />
-                    <TextInput
-                      helperText="No. of  Months"
-                      label="Waranty"
-                      name="Waranty"
-                      rules={{ min: 0 }}
-                      type="number"
-                    />
-                  </Grid>
-                </AccordionDetails>
-              </Accordion>
-              <Accordion disableGutters>
-                <AccordionSummary
-                  aria-controls="panel1a-content"
-                  expandIcon={<ExpandMoreIcon />}
-                  id="panel1a-header"
-                  sx={{
-                    backgroundColor: 'background.default',
-                  }}
-                >
-                  <Typography>Order information</Typography>
-                </AccordionSummary>
-                <AccordionDetails
-                  sx={{
-                    backgroundColor: 'background.paper',
-                  }}
-                >
-                  <Grid alignContent="center" container display="flex" spacing={2}>
-                    <TextInput label="Order Number" name="OrderNumber" rules={{}} />
-                    <DatePickerInput label="Date Of Purchase" name="DateOfPurchase" />
-                    <TextInput
-                      endAdornment="€"
-                      label="Purchase Cost"
-                      name="PurchaseCost"
-                      rules={{}}
-                      type="number"
-                    />
-                    <UploadImage buttonText="Upload Receipt Image" name="Receipt" />
-                  </Grid>
-                </AccordionDetails>
-              </Accordion>
+      {loading && <LoadingScreen displayText />}
+      {!loading && (
+        <Box>
+          {' '}
+          <Grid alignItems="center" container justifyContent="start" pt={2} spacing={0}>
+            <Grid item lg={6} md={6} sm={6} xl={6} xs={6}>
+              <Typography ml={2} variant="h4">
+                {action} Asset
+              </Typography>
             </Grid>
             <Grid
               alignContent="center"
               display="flex"
               item
               justifyContent="flex-end"
-              lg={12}
-              mb={2}
-              md={12}
-              sm={12}
-              xl={12}
-              xs={12}
+              lg={6}
+              md={6}
+              sm={6}
+              xl={6}
+              xs={6}
             >
-              <Stack direction="row" mr={2} spacing={1}>
+              <Stack direction="row" mb={1} mr={2} spacing={1}>
                 <Button color="error" onClick={() => navigate(-1)} variant="contained">
                   Cancel
                 </Button>
-                <Button color="success" type="submit" variant="contained">
+                <Button color="success" onClick={handleSubmit(onSubmit)} variant="contained">
                   {action}
                 </Button>
               </Stack>
             </Grid>
           </Grid>
-        </form>
-      </FormProvider>
+          <Divider />
+          <FormProvider {...methods}>
+            <form
+              onSubmit={handleSubmit(onSubmit)}
+              style={{
+                width: '100%',
+              }}
+            >
+              <Grid alignContent="center" container display="flex" item mt={2} spacing={2}>
+                <TextInput
+                  label="Asset Tag"
+                  name="AssetTag"
+                  rules={{ required: 'Required value' }}
+                />
+                <TextInput label="Serial" name="Serial" rules={{ required: 'Required value' }} />
+                <SelectInput
+                  label="Model"
+                  name="Model"
+                  containsImg
+                  options={modelOptions ? modelOptions : []}
+                />
+                <SelectInput
+                  label="Status"
+                  name="Status"
+                  options={statusOptions ? statusOptions : []}
+                />
+                <Grid item lg={12} md={12} sm={12} xl={12} xs={12}>
+                  <Accordion disableGutters>
+                    <AccordionSummary
+                      aria-controls="panel1a-content"
+                      expandIcon={<ExpandMoreIcon />}
+                      id="panel1a-header"
+                      sx={{
+                        backgroundColor: 'background.default',
+                      }}
+                    >
+                      <Typography>Additional informations</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails
+                      sx={{
+                        backgroundColor: 'background.paper',
+                      }}
+                    >
+                      <Grid alignContent="center" container display="flex" spacing={2}>
+                        <MultiLineTextInput label="Notes" name="Notes" rows={4} />
+                        <UploadImage buttonText="Upload photo" name="Photo" />
+                        <TextInput label="Asset Name" name="AssetName" rules={{}} />
+                        <TextInput
+                          helperText="No. of  Months"
+                          label="Waranty"
+                          name="Waranty"
+                          rules={{ min: 0 }}
+                          type="number"
+                        />
+                      </Grid>
+                    </AccordionDetails>
+                  </Accordion>
+                  <Accordion disableGutters>
+                    <AccordionSummary
+                      aria-controls="panel1a-content"
+                      expandIcon={<ExpandMoreIcon />}
+                      id="panel1a-header"
+                      sx={{
+                        backgroundColor: 'background.default',
+                      }}
+                    >
+                      <Typography>Order information</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails
+                      sx={{
+                        backgroundColor: 'background.paper',
+                      }}
+                    >
+                      <Grid alignContent="center" container display="flex" spacing={2}>
+                        <TextInput label="Order Number" name="OrderNumber" rules={{}} />
+                        <DatePickerInput label="Date Of Purchase" name="DateOfPurchase" />
+                        <TextInput
+                          endAdornment="€"
+                          label="Purchase Cost"
+                          name="PurchaseCost"
+                          rules={{}}
+                          type="number"
+                        />
+                        <UploadImage buttonText="Upload Receipt Image" name="Receipt" />
+                      </Grid>
+                    </AccordionDetails>
+                  </Accordion>
+                </Grid>
+                <Grid
+                  alignContent="center"
+                  display="flex"
+                  item
+                  justifyContent="flex-end"
+                  lg={12}
+                  mb={2}
+                  md={12}
+                  sm={12}
+                  xl={12}
+                  xs={12}
+                >
+                  <Stack direction="row" mr={2} spacing={1}>
+                    <Button color="error" onClick={() => navigate(-1)} variant="contained">
+                      Cancel
+                    </Button>
+                    <Button color="success" type="submit" variant="contained">
+                      {action}
+                    </Button>
+                  </Stack>
+                </Grid>
+              </Grid>
+            </form>
+          </FormProvider>
+        </Box>
+      )}
     </Box>
   );
 };
---
+++ src/features/assets/components/__test__/EditAsset.test.tsx
@@ -0,0 +1,40 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import React from 'react';
+import { MemoryRouter, Route, Routes } from 'react-router-dom';
+import AddAsset from '../AddAsset';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import * as element from './getElemetns';
+import { QueryClientProvider } from 'react-query';
+import { getQueryClient } from 'lib/react-query';
+import { type AppProviderProps } from 'providers/types';
+
+const Provider = ({ children }: AppProviderProps) => {
+  return (
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <QueryClientProvider client={getQueryClient()}>
+        <MemoryRouter initialEntries={['/EditAsset/1']}>
+          <Routes>
+            <Route element={children} path="EditAsset/:id" />
+          </Routes>
+        </MemoryRouter>
+      </QueryClientProvider>
+    </LocalizationProvider>
+  );
+};
+
+describe('AddAsset form', () => {
+  it('should render all inputs', async () => {
+    render(
+      <Provider>
+        <AddAsset />
+      </Provider>,
+    );
+
+    await waitFor(() => {
+      expect(screen.getByText(/edit asset/i)).toBeInTheDocument();
+    });
+    const el = element.getAssetTag();
+    expect(el).toHaveValue('assetTag');
+  });
+});
