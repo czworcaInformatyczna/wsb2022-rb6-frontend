117d0d887d78a5f3edbc6d1ca52eacd4a2170f1f
Adam-Cierniak <ad.cierniak@gmail.com>
2022-10-16 11:46:47 +0200

    add react query fetcher

--- src/lib/react-query.ts
+++ src/lib/react-query.ts
@@ -1,3 +1,20 @@
-import { QueryClient } from 'react-query';
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { QueryClient, useQuery, type QueryFunctionContext } from 'react-query';
+import { apiClient } from './axios';
 const queryClient = new QueryClient();
 export const getQueryClient = () => queryClient;
+
+type QueryKey = [string, object | undefined];
+
+export const handleFetch = async <T>({ queryKey }: QueryFunctionContext<QueryKey>): Promise<T> => {
+  const [url, params] = queryKey;
+  return await apiClient.get(url, params);
+};
+
+export const useFetch = <T>(url: string | null, params?: object) => {
+  const query = useQuery<T, Error, T, QueryKey>(
+    [url!, params],
+    async ({ queryKey, meta }) => await handleFetch({ queryKey, meta }),
+  );
+  return query;
+};
