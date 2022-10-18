3c3e71a276665e8e0dab4f8b4ae9645ab8c60e39
Adam Cierniak <ad.cierniak@gmail.com>
2022-10-18 20:40:56 +0200

    add interface

--- src/providers/types/index.ts
+++ src/providers/types/index.ts
@@ -1,7 +1,31 @@
 import { type ReactNode } from 'react';
 
-
-
 export type AppProviderProps = {
   children: ReactNode;
+};
+
+export interface ICustomTheme {
+  primary: Primary;
+  secondary: Secondary;
+  background: Background;
+  text: Text;
+}
+
+type Text = {
+  primary: string;
+};
+
+type Background = {
+  paper: string;
+  default: string;
+  dark: string;
+};
+
+type Secondary = {
+  main: string;
+};
+
+type Primary = {
+  main: string;
+  contrastText: string;
 };
