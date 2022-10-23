333301f592672b70ab4fa52d4fb45e61d273a823
Adam-Cierniak <ad.cierniak@gmail.com>
2022-10-23 15:24:29 +0200

    add mutation api methods

--- src/lib/react-query.ts
+++ src/lib/react-query.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
-import { QueryClient, useQuery, type QueryFunctionContext } from 'react-query';
+import { type AxiosError, type AxiosResponse } from 'axios';
+import { QueryClient, useMutation, useQuery, type QueryFunctionContext } from 'react-query';
 import { apiClient } from './axios';
 const queryClient = new QueryClient();
 export const getQueryClient = () => queryClient;
@@ -18,4 +19,36 @@ export const useFetch = <T>(url: string | null, params?: object) => {
     async ({ queryKey, meta }) => await handleFetch({ queryKey, meta }),
   );
   return queryContext;
+};
+
+const useGenericMutation = <T>(
+  func: (data: T) => Promise<AxiosResponse<T>>,
+  url: string,
+  params?: object,
+) => {
+  return useMutation<AxiosResponse, AxiosError, T>(func, {
+    onMutate: async () => {
+      await queryClient.cancelQueries([url!, params]);
+
+      const previousData = queryClient.getQueryData([url!, params]);
+
+      return previousData;
+    },
+
+    onSettled: async () => {
+      await queryClient.invalidateQueries([url!, params]);
+    },
+  });
+};
+
+export const usePost = <T>(url: string, params?: object) => {
+  return useGenericMutation<T>(async (data) => await apiClient.post(url, data), url, params);
+};
+
+export const useDelete = <T>(url: string, params?: object) => {
+  return useGenericMutation<T>(async (id) => await apiClient.delete(`${url}/${id}`), url, params);
+};
+
+export const useUpdate = <T>(url: string, params?: object) => {
+  return useGenericMutation<T>(async (data) => await apiClient.patch<T>(url, data), url, params);
 };
--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,3 +1,5 @@
 import { API_URL } from 'config';
+import { compile } from 'path-to-regexp';
 
 export const url = (path: string): string => `${API_URL}${path}`;
+export const convertUrl = (path: string, params: object = {}) => compile(path)(params);
