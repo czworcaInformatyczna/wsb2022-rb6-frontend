3869189674304e711cb0726ca9da971aa1491b95
Wojciech Nowicki <95188715+wkn12@users.noreply.github.com>
2022-11-07 19:42:01 +0100

    licenses mock fix

--- src/features/licenses/api/licensesApi.ts
+++ src/features/licenses/api/licensesApi.ts
@@ -1,9 +1,10 @@
+import { type IDataProviderSettings } from 'features/assets';
 import { useFetch } from 'lib/react-query';
 import { apiUrl } from 'routes';
 import { type ILicenseManufacturer, type ILicenseCategory, type ILicenses } from '../types';
 
-export const useGetLicenses = () => {
-  const context = useFetch<ILicenses[]>(apiUrl.licenses);
+export const useGetLicenses = (params: IDataProviderSettings) => {
+  const context = useFetch<ILicenses[]>(apiUrl.licenses, params);
   return context;
 };
 
