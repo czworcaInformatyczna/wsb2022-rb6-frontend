cea0e0cf1e0280bfa9b04483bf11847569434c8d
Wojciech Nowicki <95188715+wkn12@users.noreply.github.com>
2022-11-18 17:31:38 +0100

    test fix

--- src/mocks/mockData.ts
+++ src/mocks/mockData.ts
@@ -17,33 +17,36 @@ export const assetStatusOptions = [
   },
 ];
 
-export const assetModelOptions = [
-  {
-    id: 1,
-    name: 'laptop',
-    img: 'https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RWWkOk?ver=01fe&q=90&m=6&h=454&w=808&b=%23FFFFFFFF&l=f&o=t&aim=true',
-  },
-  {
-    id: 2,
-    name: 'zmywarki',
-    img: 'https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RWWkOk?ver=01fe&q=90&m=6&h=454&w=808&b=%23FFFFFFFF&l=f&o=t&aim=true',
-  },
-  {
-    id: 3,
-    name: 'wiertarka',
-    img: 'https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RWWkOk?ver=01fe&q=90&m=6&h=454&w=808&b=%23FFFFFFFF&l=f&o=t&aim=true',
-  },
-  {
-    id: 0,
-    name: 'złom',
-    img: 'https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RWWkOk?ver=01fe&q=90&m=6&h=454&w=808&b=%23FFFFFFFF&l=f&o=t&aim=true',
-  },
-  {
-    id: 4,
-    name: 'Model',
-    img: 'https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RWWkOk?ver=01fe&q=90&m=6&h=454&w=808&b=%23FFFFFFFF&l=f&o=t&aim=true',
-  },
-];
+export const assetModelOptions = {
+  total: 5,
+  data: [
+    {
+      id: 1,
+      name: 'laptop',
+      img: 'https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RWWkOk?ver=01fe&q=90&m=6&h=454&w=808&b=%23FFFFFFFF&l=f&o=t&aim=true',
+    },
+    {
+      id: 2,
+      name: 'zmywarki',
+      img: 'https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RWWkOk?ver=01fe&q=90&m=6&h=454&w=808&b=%23FFFFFFFF&l=f&o=t&aim=true',
+    },
+    {
+      id: 3,
+      name: 'wiertarka',
+      img: 'https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RWWkOk?ver=01fe&q=90&m=6&h=454&w=808&b=%23FFFFFFFF&l=f&o=t&aim=true',
+    },
+    {
+      id: 0,
+      name: 'złom',
+      img: 'https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RWWkOk?ver=01fe&q=90&m=6&h=454&w=808&b=%23FFFFFFFF&l=f&o=t&aim=true',
+    },
+    {
+      id: 4,
+      name: 'Model',
+      img: 'https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RWWkOk?ver=01fe&q=90&m=6&h=454&w=808&b=%23FFFFFFFF&l=f&o=t&aim=true',
+    },
+  ],
+};
 
 export const assetDetailsEdit = {
   id: 1,
